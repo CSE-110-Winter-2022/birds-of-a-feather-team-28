e2ea91d3198b65fcb1c35ff348978ac3afef2670
devinjokeefe <35241753+devinjokeefe@users.noreply.github.com>
2022-02-10 13:20:06 -0800

    Finalising Nearby Messages Functionality

---
+++ app/src/main/java/com/example/bof_group_28/utility/services/NearbyStudentsService.java
@@ -0,0 +1,16 @@
+package com.example.bof_group_28.utility.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NearbyStudentsService extends Service {
+    public NearbyStudentsService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
