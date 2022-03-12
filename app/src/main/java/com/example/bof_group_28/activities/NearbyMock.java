b243c69bc663edc73c2c83b087c6551be8fcf8e0
devinjokeefe <35241753+devinjokeefe@users.noreply.github.com>
2022-03-11 17:59:28 -0800

    Adding work on UI Nearby Test

---
+++ app/src/main/java/com/example/bof_group_28/activities/NearbyMock.java
@@ -0,0 +1,14 @@
+package com.example.bof_group_28.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NearbyMock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby_mock);
+    }
+}
